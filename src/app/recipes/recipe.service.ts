ea28f6d883bb47317774437db4b0c0bc3cbccb14
Susan <wangjm323@gmail.com>
2017-04-04 22:42:35 -0400

    Add recipe start component and refactor recipe detail component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -24,6 +24,9 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number) {
+    return this.recipes.slice()[index];
+  }
 
   addIngredientsToSL(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
