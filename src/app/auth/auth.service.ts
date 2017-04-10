ba30365ae5346bf151a79176fe481cb8d61f97b4
Susan <wangjm323@gmail.com>
2017-04-10 17:06:57 -0400

    Add signupUser function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,10 @@
+import * as firebase from 'firebase';
+
 export class AuthService {
   signupUser(email: string, psd: string) {
-
+    firebase.auth().createUserWithEmailAndPassword(email, psd)
+      .catch(
+        error => console.log(error)
+      );
   }
 }
