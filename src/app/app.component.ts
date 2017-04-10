4d3f6a839c951e95e5d1f2766ade39d8a35db7c2
Susan <wangjm323@gmail.com>
2017-04-10 16:57:28 -0400

    Set up firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDxWvGsh--5WuTJW8Qc9K29Js-PNGrROLk',
+      authDomain: 'ng-recipe-book-a9aed.firebaseapp.com'
+    });
+  }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,5 @@
+export class AuthService {
+  signupUser(email: string, psd: string) {
+
+  }
+}
