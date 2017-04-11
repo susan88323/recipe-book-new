562a333653a6927db6fbd3b811dd6d2ebe0eba6d
Susan <wangjm323@gmail.com>
2017-04-10 22:44:21 -0400

    Add AuthGuard service to restrict new and edit recipe actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase';
+import {Router} from '@angular/router';
+import {Injectable} from '@angular/core';
 
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(private router: Router) {}
 
   signupUser(email: string, psd: string) {
     firebase.auth().createUserWithEmailAndPassword(email, psd)
@@ -14,6 +19,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, psd)
       .then(
         response => {
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getToken()
             .then(
               (token: string) => this.token = token
